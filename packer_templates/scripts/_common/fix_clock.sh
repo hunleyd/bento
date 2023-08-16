a261a6da2f6333f9f82f4dd566b820dc06f95b56
Douglas J Hunley <doug.hunley@gmail.com>
2023-08-16 09:44:14 -0400

    fix: install chrony everywhere and allow makestep always

--- packer_templates/scripts/_common/fix_clock.sh
+++ packer_templates/scripts/_common/fix_clock.sh
@@ -7,7 +7,16 @@ if [ -f /etc/os-release ]; then
     if [ "${ID}" == 'ubuntu' ]; then
         echo "Installing chrony"
         apt -y install chrony
+        echo "Adjusting chrony config"
+        sed -i 's/3$/-1' /etc/chrony.conf
         echo "Starting chrony"
         systemctl enable --now chrony
+    else
+        echo "Installing chrony"
+        yum -y install chrony
+        echo "Adjusting chrony config"
+        sed -i 's/3$/-1' /etc/chrony.conf
+        echo "Starting chrony"
+        systemctl enable --now chronyd
     fi
 fi
