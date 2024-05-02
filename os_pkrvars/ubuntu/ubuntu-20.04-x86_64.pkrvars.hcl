23a8abbaa93905e73c3be6d33c7a79690c4891e0
Douglas J Hunley <doug.hunley@gmail.com>
2024-05-02 12:49:23 +0000

    fix: tweak motd script
    Signed-off-by: Douglas J Hunley <doug.hunley@gmail.com>

--- os_pkrvars/ubuntu/ubuntu-20.04-x86_64.pkrvars.hcl
+++ os_pkrvars/ubuntu/ubuntu-20.04-x86_64.pkrvars.hcl
@@ -6,5 +6,4 @@ iso_checksum            = "file:https://releases.ubuntu.com/focal/SHA256SUMS"
 parallels_guest_os_type = "ubuntu"
 vbox_guest_os_type      = "Ubuntu_64"
 vmware_guest_os_type    = "ubuntu-64"
-boot_wait               = "4s"
 boot_command            = ["<wait><enter><wait><enter><wait><f6><wait><esc><wait> autoinstall ds=nocloud-net;seedfrom=http://{{ .HTTPIP }}:{{ .HTTPPort }}/ubuntu/<enter><wait>"]
--- packer_templates/scripts/_common/motd.sh
+++ packer_templates/scripts/_common/motd.sh
@@ -1,16 +1,19 @@
 #!/bin/sh -eux
 
-touch /etc/motd
-chmod 0777 /etc/motd
-cat << 'BENTO' >> /etc/motd
+    MOTD_CONFIG='/etc/motd'
 
-This system is built by the Crunchy Postgres Automation project at
-                            _               _       _
-  ___ _ __ _   _ _ __   ___| |__  _   _  __| | __ _| |_ __ _
- / __| '__| | | | '_ \ / __| '_ \| | | |/ _` |/ _` | __/ _` |
-| (__| |  | |_| | | | | (__| | | | |_| | (_| | (_| | || (_| |
- \___|_|   \__,_|_| |_|\___|_| |_|\__, |\__,_|\__,_|\__\__,_|
-                                  |___/
+    touch $MOTD_CONFIG
+    chmod 0777 $MOTD_CONFIG
+    cat << 'BENTO' >> $MOTD_CONFIG
+
+    This system is built by the Crunchy Postgres Automation project at
+                                _               _       _
+      ___ _ __ _   _ _ __   ___| |__  _   _  __| | __ _| |_ __ _
+     / __| '__| | | | '_ \ / __| '_ \| | | |/ _` |/ _` | __/ _` |
+    | (__| |  | |_| | | | | (__| | | | |_| | (_| | (_| | || (_| |
+     \___|_|   \__,_|_| |_|\___|_| |_|\__, |\__,_|\__,_|\__\__,_|
+                                      |___/
 
 BENTO
-chmod 0755 /etc/motd
+
+chmod 0755 $MOTD_CONFIG
